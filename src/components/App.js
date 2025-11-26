088e0a66342f34fea495d1542941134c70b5518d
kittu2karthik <kittu2karthik@gmail.com>
2025-11-26 11:19:27 +0530

    Added Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
+    <div id="main" className="ns-wrapper">
+      <h1 data-ns-test="project-name">
+        What is Lorem Ipsum?What is Lorem Ipsum?
+      </h1>
 
+      <h6 data-ns-test="project-description">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a
+      </h6>
+    </div>
+  );
+};
 
 export default App;
